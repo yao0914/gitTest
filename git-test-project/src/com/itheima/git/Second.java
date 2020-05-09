67044122c79b1569f13252072062e2fea5892873
zhangyao <zhangyao@qq.com>
2020-05-09 14:42:12 +0800

    添加second

---
+++ git-test-project/src/com/itheima/git/Second.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+/**
+ * @author zhangyao
+ * @date 2020/5/9 14:41
+ */
+public class Second {
+    public void test1() {
+        System.out.println("hello second");
+    }
+}
